f98dda0f01ff33b4cf61d5c85f7c183311e04098
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2017-09-27 23:54:41 +0200

    Improved CameraManager to be able to stream video on provided socket

--- alexapi/utils.py
+++ alexapi/utils.py
@@ -1,8 +1,21 @@
 import picamera
 
 class CameraManager:
-    def __init__(self):
-        self.camera=picamera.PiCamera()
-    def take_picture(self):
-        self.camera.capture('tmp.jpg')
-    
+	def __init__(self):
+		self.camera=picamera.PiCamera()
+		self.socket=None
+		self.conn=None
+	def take_picture(self):
+		self.camera.capture('tmp.jpg')
+	def start_video_stream(socket):
+		self.socket = socket
+		self.conn = socket.accept()[0].makefile('f')
+		self.camera.start_recording(conn, format='h264')
+	def stop_video_stream():
+		try:
+			self.camera.stop_recording()
+		finally:
+			self.conn.close()
+			self.conn = None
+			self.socket.close()
+			self.socket = None
