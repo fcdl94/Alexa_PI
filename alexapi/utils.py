227f468ec1eb81a8ff56cc710dda839e7196bbe5
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2017-09-28 13:53:47 +0200

    Added my Copyright notice

--- alexapi/utils.py
+++ alexapi/utils.py
@@ -1,3 +1,18 @@
+# Copyright (c) Gianmarco Garrisi 2017
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 import picamera
 import socket
 
@@ -7,6 +22,7 @@ class CameraManager:
 		self.sock=soc
 		self.conn=None
 	def take_picture(self):
+                self.camera.resultion = (1024,768)
 		self.camera.capture('tmp.jpg')
 	def start_video_stream(self):
                 self.camera.resolution = (640, 480)
