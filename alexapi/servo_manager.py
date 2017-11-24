f1dd9e3f1bc6f2ae5adf2a280a633c3a00a2eda8
damianofranzo <33485956+damianofranzo@users.noreply.github.com>
2017-11-24 15:25:20 +0100

    questo è una bozza del servo manager che servirà per il movimento

--- alexapi/servo_manager.py
+++ alexapi/servo_manager.py
@@ -1,12 +1,28 @@
-#in questo file verrà creata una classe che si occupa di gestire il movimento attraverso una classe wrapper
-#la libreria che verrà usata è adafruit pca9685
+#!/usr/bin/env python
+
 import Adafruit_PCA9685 as pca
+import time
 
 class servomanager():
-    #funzione esempio
+    def __init__(self,input_address,bus_number):
+        self.pwm=pca.PCA9685(address=input_address,busnum=bus_number)
+        self.pwm.set_pwm_freq(60)
+        self.servo_min=150
+        self.servo_max=600
     def movto(self,position):
-        #viene passata la possizione in cui il servo deve ruotare
-        #nella funzione init verra specificato quanti punti cardinali tenere in considerazino
-    def __init__(self):
-        #questa funzione avrà lo scopo di inizializzare il driver per il servo
-        pwm=pca.PCA9685(address=0x54,busnum=1)
+        if(position==1):
+            self.pwm.set_pwm(0, 0, self.servo_min)
+            time.sleep(1)
+        if(position==2):
+            self.pwm.set_pwm(0,0,(self.servo_min+self.servo_max)/2)
+            time.sleep(1)
+        else:
+            self.pwm.set_pwm(0,0,self.servo_max)
+            time.sleep(1)
+
+servo=servomanager(0x54,1)
+servo.movto(1)
+servo.movto(3)
+servo.movto(2)
+servo.movto(3)
+servo.movto(1)
