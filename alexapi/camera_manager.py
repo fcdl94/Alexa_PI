1b1e01db2af05a5786d0c1183023c059c52fa32d
Gianmarco Garrisi <gianmarcogarrisi@tutanota.com>
2017-11-25 18:26:46 +0100

    Resolved dependencies. Fixed some indentation inconsistency issues

--- alexapi/camera_manager.py
+++ alexapi/camera_manager.py
@@ -17,21 +17,21 @@ import picamera
 import socket
 
 class CameraManager:
-	def __init__(self, soc):
-		self.camera=picamera.PiCamera()
-		self.sock=soc
-		self.conn=None
-	def take_picture(self):
+        def __init__(self, soc):
+                self.camera=picamera.PiCamera()
+                self.sock=soc
+                self.conn=None
+        def take_picture(self):
                 self.camera.resultion = (1024,768)
-		self.camera.capture('tmp.jpg')
-	def start_video_stream(self):
+                self.camera.capture('tmp.jpg')
+        def start_video_stream(self):
                 self.camera.resolution = (640, 480)
                 self.camera.framerate = 24
-		self.conn = self.sock.makefile(mode='wb')
-		self.camera.start_recording(self.conn, format='h264')
-	def stop_video_stream(self):
-		try:
-			self.camera.stop_recording()
-		finally:
-			self.conn.close()
-			self.conn = None
+                self.conn = self.sock.makefile(mode='wb')
+                self.camera.start_recording(self.conn, format='h264')
+        def stop_video_stream(self):
+                try:
+                        self.camera.stop_recording()
+                finally:
+                        self.conn.close()
+                        self.conn = None
